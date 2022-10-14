7f6db2d85221e8b4422203f3163d393f487e76cc
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-10-14 14:25:09 +0530

    new id created for canvas art

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
