1cce8ee73702234e9d0d45ada744048455e82db1
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-10-14 22:57:37 +0530

    editing affects with transalte and rotate controls

--- main.js
+++ main.js
@@ -7,15 +7,17 @@ window.addEventListener('load', function(){
     // canvas settings
     ctx.fillStyle = 'yellow';
     ctx.strokeStyle = 'blueviolet';
-    ctx.lineWidth = 30;
+    ctx.lineWidth = 25;
     ctx.lineCap = 'round';
 
     // effect settings
     const size = 200;
-
-    ctx.translate(100,100)
-    ctx.rotate(0.3);
+    ctx.save();
+    ctx.translate(canvas.width/2,200);
+    ctx.scale(1,1);
+    ctx.rotate(0);
     ctx.fillRect(0,0,canvas.width, canvas.height);
+    ctx.restore();
     ctx.beginPath();    // start drawing a new shape
     ctx.moveTo(canvas.width/2,canvas.height/2);    // set starting x and y co-ordinates of a new shape
     ctx.lineTo(size,canvas.height/2);   // set ending co-ordinates of a line
