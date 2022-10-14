dfdab20d01c714fec9d690d7b08008984768a450
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-10-14 17:11:52 +0530

    drawed a new shape with their settings

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    // canvas settings
+    ctx.fillStyle = 'yellow';
+    ctx.strokeStyle = 'lime';
+    ctx.fillRect(50,50,100,100);
+
+    ctx.beginPath();      // start drawing a new shape
+    ctx.moveTo(300,300);    // set starting x and y co-ordinates of a new shape
+    ctx.lineTo(400, 400);   // set ending co-ordinates of a line
+    ctx.stroke();   // outlines the current path with the current stroke style
 });
