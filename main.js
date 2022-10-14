4eec1ff8343f54b43c65bc6a527fb2281458d77a
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-10-14 19:42:28 +0530

    stroke added

--- main.js
+++ main.js
@@ -7,10 +7,15 @@ window.addEventListener('load', function(){
     // canvas settings
     ctx.fillStyle = 'yellow';
     ctx.strokeStyle = 'lime';
+    ctx.lineWidth = 30;
+    ctx.lineCap = 'round';
+
+    
+
     ctx.fillRect(50,50,100,100);
 
-    ctx.beginPath();      // start drawing a new shape
-    ctx.moveTo(300,300);    // set starting x and y co-ordinates of a new shape
-    ctx.lineTo(400, 400);   // set ending co-ordinates of a line
+    ctx.beginPath();    // start drawing a new shape
+    ctx.moveTo(canvas.width/2,canvas.height/2);    // set starting x and y co-ordinates of a new shape
+    ctx.lineTo(canvas.width/2 - 300,canvas.height/2);   // set ending co-ordinates of a line
     ctx.stroke();   // outlines the current path with the current stroke style
 });
