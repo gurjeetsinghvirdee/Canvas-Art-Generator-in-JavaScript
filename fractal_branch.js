c782233e258c894be4ced30fd9bb642d3ddfec8b
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-10-17 01:10:57 +0530

    created a new file drawfractal

--- fractal_branch.js
+++ fractal_branch.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth * 1;
-    canvas.height = window.innerHeight * 1.3;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
     // canvas settings
     ctx.fillStyle = 'yellow';
@@ -12,21 +12,49 @@ window.addEventListener('load', function(){
     // effect settings
     let size = 170;
     let sides = 10;
+    let maxLevel = 3;
+    let scale = 0.6;
+    let spread = 0.8;
+    let branches = 2;
     ctx.save();
     ctx.translate(canvas.width/2,canvas.height/2);
     ctx.scale(1,1);
     ctx.rotate(0);
     // ctx.fillRect(0,0,canvas.width, canvas.height);
+
+    // Fractal Art
     
-    for (let i = 0; i < sides; i++){
+    function drawBranch(level){
+        if (level > maxLevel) return;
         ctx.beginPath();    // start drawing a new shape
         ctx.moveTo(0,0);    // set starting x and y co-ordinates of a new shape
         ctx.lineTo(size,0);   // set ending co-ordinates of a line
         ctx.stroke();   // outlines the current path with the current stroke style
-        ctx.rotate((Math.PI * 2)/sides);    // angle values in radians
-        ctx.scale(0.97, 0.97);
-        ctx.translate(30,50)
+        
+        for (let i = 0; i < branches; i++){
+            ctx.save();
+            ctx.translate(size - (size/branches) * i,0);
+            ctx.rotate(spread);
+            ctx.scale(scale,scale);
+            drawBranch(level + 1);
+            ctx.restore();
+
+            ctx.save();
+            ctx.translate(size - (size/branches) * i,0);
+            ctx.rotate(-spread);
+            ctx.scale(scale,scale);
+            drawBranch(level + 1);
+            ctx.restore();
+        }
     }
+    drawBranch(0);
+
+
+    // for (let i = 0; i < sides; i++){
+        
+    //     ctx.rotate((Math.PI * 2)/sides);    // angle values in radians
+    //     
+    // }
     
     ctx.restore()
 });
