c591d3c9943b6489a7f9177c882a7feaa28c6fe9
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-10-16 19:40:32 +0530

    angle values in radius

--- main.js
+++ main.js
@@ -6,20 +6,26 @@ window.addEventListener('load', function(){
 
     // canvas settings
     ctx.fillStyle = 'yellow';
-    ctx.strokeStyle = 'blueviolet';
+    ctx.strokeStyle = 'lime';
     ctx.lineWidth = 25;
     ctx.lineCap = 'round';
 
     // effect settings
-    const size = 200;
+    let size = 200;
+    let sides = 7;
     ctx.save();
-    ctx.translate(canvas.width/2,200);
+    ctx.translate(canvas.width/2,canvas.height/2);
     ctx.scale(1,1);
     ctx.rotate(0);
-    ctx.fillRect(0,0,canvas.width, canvas.height);
-    ctx.restore();
-    ctx.beginPath();    // start drawing a new shape
-    ctx.moveTo(canvas.width/2,canvas.height/2);    // set starting x and y co-ordinates of a new shape
-    ctx.lineTo(size,canvas.height/2);   // set ending co-ordinates of a line
-    ctx.stroke();   // outlines the current path with the current stroke style
+    // ctx.fillRect(0,0,canvas.width, canvas.height);
+    
+    for (let i = 0; i < sides; i++){
+        ctx.beginPath();    // start drawing a new shape
+        ctx.moveTo(0,0);    // set starting x and y co-ordinates of a new shape
+        ctx.lineTo(size,0);   // set ending co-ordinates of a line
+        ctx.stroke();   // outlines the current path with the current stroke style
+        ctx.rotate((Math.PI * 2)/sides);    // angle values in radians
+    }
+    
+    ctx.restore()
 });
